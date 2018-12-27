dbf9382a3adad6fb9391cc7871bd2d7e3fcf0dd7
ama <a.mariyanna@castsoftware.com>
2018-12-27 16:18:44 +0530

    Iterator concepts

---
+++ functional/src/bin/counter.rs
@@ -0,0 +1,80 @@
+struct Counter {
+    count: u32,
+}
+
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+        
+        assert_eq!(18, sum);
+    }
+}
+
+fn main() {
+    println!("Hi, Run tests");
+
+    for i in Counter::new() {
+        println!("{}", i);
+    }
+    println!("\n");
+
+    for i in Counter::new().zip(Counter::new().skip(1)) {
+        println!("{:?}", i);
+    }
+    println!("\n");
+
+    for i in Counter::new().zip(Counter::new().skip(1)).map(|(a,b)| a*b) {
+        println!("{:?}", i);
+    }
+    println!("\n");
+
+    for i in Counter::new().zip(Counter::new().skip(1)).map(|(a,b)| a*b).filter(|a| a%3 == 0) {
+        println!("{:?}", i);
+    }
+    println!("\n");
+
+    let s : u32 = Counter::new().zip(Counter::new().skip(1)).map(|(a,b)| a*b).filter(|a| a%3 == 0).sum();
+    println!("{}", s);
+
+    println!("{}", Counter::new().zip(Counter::new().skip(1)).map(|(a,b)| a*b).filter(|a| a%3 == 0).sum::<u32>()); // Turbofish
+}
