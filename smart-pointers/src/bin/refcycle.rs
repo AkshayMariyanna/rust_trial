b681c1507bbda5f91a0090549904eacdc3563ed9
Akshay Mariyanna <akshay.mariyanna@gmail.com>
2019-01-09 19:25:38 +0530

    RefCell

---
+++ smart-pointers/src/bin/refcycle.rs
@@ -0,0 +1,32 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+use self::List::{Cons, Nil};
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
+}
